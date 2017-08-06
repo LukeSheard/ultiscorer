f6d7590c043b109b37b4fca8e697ead1457eb9ba
Luke Sheard <me@lukesheard.com>
2017-08-06 20:32:11 +0100

    Try and fix chrome headers issue for fonts

--- config/webpack/webpack:base.ts
+++ config/webpack/webpack:base.ts
@@ -7,6 +7,11 @@ export default function(env: string) {
     context: join(__dirname, "../../"),
     devServer: {
       headers: {
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Headers":
+          "X-Requested-With, content-type, Authorization",
+        "Access-Control-Allow-Methods":
+          "GET, POST, PUT, DELETE, PATCH, OPTIONS",
         "Access-Control-Allow-Origin": "*"
       }
     },
