29ca4c6cdd8e66ce2c4a540105b3b9e164eab85a
Luke Burgess <luke.burgess94@gmail.com>
2017-08-09 22:47:58 +0100

    Added content to home page

--- src/common/pages/home.tsx
+++ src/common/pages/home.tsx
@@ -1,7 +1,40 @@
+import { Button, Intent } from "@blueprintjs/core"
 import * as React from "react";
+import { Link } from "react-router";
 
 export default class NotFoundPage extends React.Component<any, any> {
   public render() {
-    return <h1>Ultiscorer</h1>;
+    return (
+        <div>
+            <h1>Welcome To Ultiscorer</h1>
+            <p>Sign-in or sign-up using the buttons below!</p>
+            <div
+            style={{
+                textAlign: "center"
+            }}
+            >
+                <Link to="sign-in">
+                  <Button
+                    style={{
+                        width: "50%"
+                    }}
+                    intent={Intent.SUCCESS}
+                    text="Sign-in"
+                  />
+                </Link>
+                <br/>
+                <br/>
+                <Link to="sign-up">
+                  <Button
+                    style={{
+                        minWidth: "50%"
+                    }}
+                    intent={Intent.PRIMARY}
+                    text="Sign-up"
+                  />
+                </Link>
+            </div>
+        </div>
+    )
   }
 }
