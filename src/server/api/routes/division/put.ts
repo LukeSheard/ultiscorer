cf51a446f61be28e3575dbe8c1826f3186fe629a
Luke Sheard <me@lukesheard.com>
2017-08-20 12:38:40 +0000

    Fix tests

--- src/server/api/routes/division/put.ts
+++ src/server/api/routes/division/put.ts
@@ -1,38 +1,38 @@
 import { Request, Response } from "express";
-import Division from "../../../models/division";
+import Division from "../../../../models/division";
 
 export default function(req: Request, res: Response) {
-    return Division.findById(req.params.id)
-        .then(division => {
-            if (!division){
-                throw Error("Division does not exist.");
-            }
+  return Division.findById(req.params.id)
+    .then(division => {
+      if (!division) {
+        throw Error("Division does not exist.");
+      }
 
-            const { name, teams } = req.body;
+      const { name, teams } = req.body;
 
-            if (name) {
-                division.name = name;
-            }
+      if (name) {
+        division.name = name;
+      }
 
-            if (teams) {
-                division.teams = teams;
-            }
+      if (teams) {
+        division.teams = teams;
+      }
 
-            return division.save();
-        })
-        .then(division => {
-            return res.json({
-                data: {
-                    attributes: division,
-                    id: division._id,
-                    type: "Division"
-                }
-            });
-        })
-        .catch(error => {
-            res.status(500);
-            return res.json({
-                error
-            });
-        });
+      return division.save();
+    })
+    .then(division => {
+      return res.json({
+        data: {
+          attributes: division,
+          id: division._id,
+          type: "Division"
+        }
+      });
+    })
+    .catch(error => {
+      res.status(500);
+      return res.json({
+        error
+      });
+    });
 }
